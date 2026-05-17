6816cb3b21fb52f6da4741f7d95823da734fa857
jamillebez <jaahmartins6@gmail.com>
2026-05-17 20:39:48 -0300

    feat: implementa os 4 métodos remotos exigidos

--- src/br/ufc/quixada/reserva/app/ClienteEspacoFisico.java
+++ src/br/ufc/quixada/reserva/app/ClienteEspacoFisico.java
@@ -5,34 +5,43 @@ import br.ufc.quixada.reserva.rmi.MiddlewareRMI;
 import com.google.gson.Gson;
 
 public class ClienteEspacoFisico {
+    
+    private static MiddlewareRMI middleware;
+    private static String ipServidor = "localhost";
+    private static int portaServidor = 12345;
+
     public static void main(String[] args) {
         try {
-            MiddlewareRMI middleware = new MiddlewareRMI();
+            middleware = new MiddlewareRMI(); 
             Gson gson = new Gson();
 
             EspacoFisico espaco = new Sala(1, "Sala UFC Quixadá", 50, true);
-            
             String argumentosJSON = gson.toJson(espaco);
             
-            System.out.println("Invocando método remotamente via RMI...");
-            
-            byte[] respostaBytes = middleware.doOperation(
-                "ServicoReserva",        
-                "solicitarReserva",     
-                argumentosJSON.getBytes(), 
-                "localhost",            
-                12345                    
-            );
+            System.out.println("--- TESTANDO INVOCACAO RMI (4 MÉTODOS) ---\n");
 
-            if (respostaBytes != null) {
-                String resposta = new String(respostaBytes).trim();
-                System.out.println("Resposta do Servidor RMI: " + resposta);
-            } else {
-                System.out.println("Erro: Nenhuma resposta do servidor.");
-            }
+            invocarMetodo("ServicoReserva", "solicitarReserva", argumentosJSON);
+
+            invocarMetodo("ServicoReserva", "cancelarReserva", "{\"idReserva\": 1}");
+
+            invocarMetodo("ServicoConsulta", "verificarDisponibilidade", argumentosJSON);
+
+            invocarMetodo("ServicoConsulta", "consultarDetalhes", argumentosJSON);
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void invocarMetodo(String objectRef, String methodId, String argumentos) {
+        System.out.println(">> Invocando remotamente: " + methodId);
+        byte[] respostaBytes = middleware.doOperation(objectRef, methodId, argumentos.getBytes(), ipServidor, portaServidor);
+
+        if (respostaBytes != null) {
+            String resposta = new String(respostaBytes).trim();
+            System.out.println("   Resposta do Servidor: " + resposta + "\n");
+        } else {
+            System.out.println("   Erro: Nenhuma resposta.\n");
         }
     }
 }
--- src/br/ufc/quixada/reserva/app/ServidorEspacoFisico.java
+++ src/br/ufc/quixada/reserva/app/ServidorEspacoFisico.java
@@ -11,20 +11,32 @@ public class ServidorEspacoFisico {
             System.out.println("Servidor RMI funcionando na porta 12345...");
 
             while (true) {
-                System.out.println("Aguardando requisições...");
+                System.out.println("\nAguardando requisições...");
                 
                 DatagramPacket pacoteRecebido = middleware.getRequest();
                 MensagemRPC msgRequisicao = MensagemRPC.fromBytes(pacoteRecebido.getData());
 
                 System.out.println("Método invocado: " + msgRequisicao.methodId);
+                System.out.println("Argumentos recebidos: " + msgRequisicao.arguments);
                 
                 String resposta;
                 
-                if (msgRequisicao.methodId.equals("solicitarReserva")) {
-                    System.out.println("Argumentos recebidos: " + msgRequisicao.arguments);
-                    resposta = "Reserva confirmada com sucesso no servidor!";
-                } else {
-                    resposta = "Método não encontrado.";
+                switch (msgRequisicao.methodId) {
+                    case "solicitarReserva":
+                        resposta = "Sucesso: Reserva confirmada no servidor!";
+                        break;
+                    case "cancelarReserva":
+                        resposta = "Sucesso: Reserva cancelada no servidor!";
+                        break;
+                    case "verificarDisponibilidade":
+                        resposta = "Info: O espaço encontra-se disponível para a data solicitada.";
+                        break;
+                    case "consultarDetalhes":
+                        resposta = "Detalhes: " + msgRequisicao.arguments; 
+                        break;
+                    default:
+                        resposta = "Erro: Método remoto não encontrado.";
+                        break;
                 }
 
                 middleware.sendReply(resposta.getBytes(), pacoteRecebido.getAddress(), pacoteRecebido.getPort());
